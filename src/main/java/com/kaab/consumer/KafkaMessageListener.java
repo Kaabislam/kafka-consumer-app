f618035b5f3f818742a6a477f9de88d539c14d98
kaab <kaab@mpower-social.com>
2024-12-08 11:34:13 +0600

    change configuration from yml to java class

--- src/main/java/com/kaab/consumer/KafkaMessageListener.java
+++ src/main/java/com/kaab/consumer/KafkaMessageListener.java
@@ -1,5 +1,6 @@
 package com.kaab.consumer;
 
+import com.kaab.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,9 +11,33 @@ public class KafkaMessageListener {
 
     Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "kaab-topic-2",groupId = "kaab-group-2")
-    public void consume(String message){
-        logger.info("Consume consume the message {}",message);
+    @KafkaListener(topics = "kaab-topic-class",groupId = "kaab-topic-class")
+    public void consumeCustomer(Customer customer){
+        logger.info("Consumer consume the event {}",customer.toString());
 
     }
+
+//    @KafkaListener(topics = "kaab-topic-2",groupId = "kaab-group-2")
+//    public void consume1(String message){
+//        logger.info("Consumer [1] consume the message {}",message);
+//
+//    }
+//
+//    @KafkaListener(topics = "kaab-topic-2",groupId = "kaab-group-2")
+//    public void consume2(String message){
+//        logger.info("Consumer [2] consume the message {}",message);
+//
+//    }
+//
+//    @KafkaListener(topics = "kaab-topic-2",groupId = "kaab-group-2")
+//    public void consum3(String message){
+//        logger.info("Consumer [3] consume the message {}",message);
+//
+//    }
+//
+//    @KafkaListener(topics = "kaab-topic-2",groupId = "kaab-group-2")
+//    public void consume4(String message){
+//        logger.info("Consumer [4] consume the message {}",message);
+//
+//    }
 }
